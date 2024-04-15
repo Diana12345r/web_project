f4b06843c630259a7c79ada79a99866ae41a662b
Diana12345r <shagalieva.d@gmail.com>
2024-04-15 23:46:18 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, url_for, request
+import requests
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    with open('design.html', 'r', encoding='utf-8') as html_stream:
+        html = html_stream.read()
+    if request.method == "GET":
+        return html
+    elif request.method == 'POST':
+        f = request.files['file']
+        print(f.read())
+        return "Форма отправлена"
+
+
+if __name__ == '__main__':
+    app.run(port=8000, debug=True)
